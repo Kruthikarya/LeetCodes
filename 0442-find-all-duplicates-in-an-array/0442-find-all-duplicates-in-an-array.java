b18bee470260441a23480df5a37d9fdd326d67d9
Kruthik Arya B M <gg8468017@gmail.com>
2026-07-02 22:59:29 +0530

    Time: 6 ms (89.68%), Space: 57.9 MB (49.05%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -4,20 +4,15 @@ class Solution {
         List<Integer> resultset= new ArrayList<>();
         for(int i=0;i<nums.length;i++){
             int index=Math.abs(nums[i])-1;
-
             if(nums[index]<0){
-                //if it is -ve the you have already madd it, so add it into results
                 resultset.add(index+1);
             }else{
-        
-
-
-        //if not -ve and it is first occured then make it negative
-          nums[index]=nums[index]*-1;
+                nums[index]=nums[index]*-1;
             }
-              
+            
         }
-         return resultset;
+        return resultset;
+      
      
     }
 }
