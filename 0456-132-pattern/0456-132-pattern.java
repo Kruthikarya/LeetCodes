c0b6df7aea5c959fc24339f1290b4d447d1201ea
Kruthik Arya B M <gg8468017@gmail.com>
2026-08-31 22:00:00 +0530

    Time: 38 ms (65.09%), Space: 98.9 MB (30.21%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -1,17 +1,15 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
 
-
-        Stack<Integer> st= new Stack<>();
-        int thrid= Integer.MIN_VALUE;
-        for(int i=nums.length-1;i>=0;i--){
-            if(nums[i]<thrid) return true;
-            while( !st.isEmpty()&&st.peek()<nums[i]){
-                thrid=st.pop();
-            }
-            st.push(nums[i]);
-        }
-        return false;
-        
+Stack<Integer> st = new Stack<>();
+int third = Integer.MIN_VALUE;
+for(int i=nums.length-1;i>=0;i--){
+    if(nums[i]<third)return true;
+    while(!st.isEmpty()&&st.peek()<nums[i]){
+         third= st.pop();
     }
+    st.push(nums[i]);
+}
+return false;
+}
 }
