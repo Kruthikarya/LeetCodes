b79cf493c707eb22fdb7f123baef053f73355c8e
Kruthik Arya B M <gg8468017@gmail.com>
2026-09-04 22:58:36 +0530

    Time: 5 ms (83.23%), Space: 77 MB (95.87%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -4,16 +4,20 @@ class Solution {
         int right=height.length-1;
         int maxarea=0;
         while(left<right){
+            //cal height min
             int h=Math.min(height[left],height[right]);
+            //cal wid 
             int w=right-left;
-            maxarea= Math.max(maxarea,h*w);
+            //cal maxarea
+            maxarea=Math.max(maxarea,h*w);
             if(height[left]<height[right]){
                 left++;
             }else{
                 right--;
             }
+
+
         }
         return maxarea;
-        
     }
 }
